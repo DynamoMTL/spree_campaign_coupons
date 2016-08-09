e387af88dec8fa31fc8a9e347407e9b2b7b9e440
David Ang <davidang09@gmail.com>
2016-08-10 03:19:20 +0800

    factory for the coupon file fixture

--- lib/spree_coupons/factories.rb
+++ lib/spree_coupons/factories.rb
@@ -1,6 +1,12 @@
+include ActionDispatch::TestProcess
+
 FactoryGirl.define do
   # Define your Spree extensions Factories within this file to enable applications, and other extensions to use and override them.
   #
   # Example adding this to your spec_helper will load these Factories for use:
   # require 'spree_coupons/factories'
+
+  factory :coupon_file, class: OpenStruct do
+    file { fixture_file_upload(Rails.root.parent.to_s + "/fixtures/coupon_codes.csv") }
+  end
 end
