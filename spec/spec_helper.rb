093dab104650673ef1f16a6c8e0a0c5287e34090
David Ang <davidang09@gmail.com>
2016-08-10 03:18:25 +0800

    include sidekiq testing so we can test sidekiq workers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,6 +19,7 @@ require 'rspec/rails'
 require 'database_cleaner'
 require 'ffaker'
 require 'pry'
+require 'sidekiq/testing'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
